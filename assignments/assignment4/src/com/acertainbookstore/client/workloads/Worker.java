21281cd1ebcd16bfebead76193577bf0923c9eda
Hans <arngorf@gmail.com>
2015-12-30 17:18:01 +0100

    ehh

--- assignments/assignment4/src/com/acertainbookstore/client/workloads/Worker.java
+++ assignments/assignment4/src/com/acertainbookstore/client/workloads/Worker.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.acertainbookstore.client.workloads;
 
@@ -19,10 +19,10 @@ import com.acertainbookstore.interfaces.StockManager;
 import com.acertainbookstore.utils.BookStoreException;
 
 /**
- * 
+ *
  * Worker represents the workload runner which runs the workloads with
  * parameters using WorkloadConfiguration and then reports the results
- * 
+ *
  */
 public class Worker implements Callable<WorkerRunResult> {
 	private WorkloadConfiguration configuration = null;
@@ -36,10 +36,10 @@ public class Worker implements Callable<WorkerRunResult> {
 	/**
 	 * Run the appropriate interaction while trying to maintain the configured
 	 * distributions
-	 * 
+	 *
 	 * Updates the counts of total runs and successful runs for customer
 	 * interaction
-	 * 
+	 *
 	 * @param chooseInteraction
 	 * @return
 	 */
@@ -104,27 +104,39 @@ public class Worker implements Callable<WorkerRunResult> {
 
 	/**
 	 * Runs the new stock acquisition interaction
-	 * 
+	 *
 	 * @throws BookStoreException
 	 */
 	private void runRareStockManagerInteraction() throws BookStoreException {
-		
+
 		// Get StockManager and BookSetGenerator from configuration file.
 		StockManager stm = configuration.getStockManager();
 		BookSetGenerator bookGen = configuration.getBookSetGenerator();
-		
+
+		if (stm == null) {
+			throw new BookStoreException("StockManager is null");
+		}
+
+		if (bookGen == null) {
+			throw new BookStoreException("BookSetGenerator is null");
+		}
+
 		// Get all books from bookstore.
 		List<StockBook> storeBooks = stm.getBooks();
-		
+
+		if (storeBooks == null) {
+			throw new BookStoreException("storeBooks is null");
+		}
+
 		Set<StockBook> randomBookSet = bookGen.nextSetOfStockBooks(configuration.getNumBooksToAdd());
 		Set<Integer> isbnOfRandom = new HashSet<Integer>();
 
 		for (StockBook book : randomBookSet) {
 			isbnOfRandom.add(book.getISBN());
 		}
-		
+
 		Set<StockBook> booksNotFound = new HashSet<StockBook>();
-		
+
 		// Check if books are in the bookstore.
 		for (StockBook book : storeBooks) {
 			if (!isbnOfRandom.contains(book.getISBN())) {
@@ -136,17 +148,25 @@ public class Worker implements Callable<WorkerRunResult> {
 
 	/**
 	 * Runs the stock replenishment interaction
-	 * 
+	 *
 	 * @throws BookStoreException
 	 */
 	private void runFrequentStockManagerInteraction() throws BookStoreException {
-		
+
 		// Get StockManager from configuration file.
 		StockManager stm = configuration.getStockManager();
-		
+
+		if (stm == null) {
+			throw new BookStoreException("StockManager is null");
+		}
+
 		// Get all books from bookstore.
 		List<StockBook> storeBooks = stm.getBooks();
-		
+
+		if (storeBooks == null) {
+			throw new BookStoreException("storeBooks is null");
+		}
+
 		// Sort books on number of copies
 		Collections.sort(storeBooks, new Comparator<StockBook>() {
 			public int compare(StockBook a, StockBook b) {
@@ -171,21 +191,25 @@ public class Worker implements Callable<WorkerRunResult> {
 
 	/**
 	 * Runs the customer interaction
-	 * 
+	 *
 	 * @throws BookStoreException
 	 */
 	private void runFrequentBookStoreInteraction() throws BookStoreException {
-		
+
 		// Get BookStore from configuration file.
 		BookStore bkst = configuration.getBookStore();
-		
-		// Get random number of books to which we want the smallest number of copies.	
-		
+
+		if (bkst == null) {
+			throw new BookStoreException("BookStore is null");
+		}
+
+		// Get random number of books to which we want the smallest number of copies.
+
 		List<Book> editorPicks = bkst.getEditorPicks(configuration.getNumEditorPicksToGet())
 													 .subList(0, configuration.getNumBooksToBuy());
-		
+
 		Set<BookCopy> booksToBuy = new HashSet<BookCopy>();
-				
+
 		for (Book book : editorPicks) {
 			booksToBuy.add( new BookCopy(book.getISBN(), configuration.getNumBookCopiesToBuy()) );
 		}
